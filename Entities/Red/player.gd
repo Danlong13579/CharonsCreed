6770821965b4235d726309be94cd4c068c7fd305
Daniel Long <Danlong13579@gmail.com>
2024-09-20 16:47:40 -0400

    added phantom cam plugin

--- Entities/Red/player.gd
+++ Entities/Red/player.gd
@@ -4,18 +4,10 @@ var moveSpeed := 6000
 var dashSpeed := 20000
 const dashDuration = 0.2
 
-@onready var animated_sprite = $AnimatedSprite2D
-@onready var dash = $Dash
-
 func _physics_process(delta):
-	dash.set_cooldown(1.0)
 	var inputDirection = Input.get_vector("move_left","move_right","move_up","move_down").normalized()
-	
-	#if Input.is_action_just_pressed("dash"):
-		#if !dash.is_dashing():
-			#dash.start_dash(dashDuration, animated_sprite)
 		
-	var speed = dashSpeed if dash.is_dashing() else moveSpeed
+	var speed = moveSpeed
 	
 	velocity = inputDirection * speed * delta
 	move_and_slide()
